30fb4688512ce0f0cb66456fef0b033326cc6114
Joe Hermaszewski <git@monoid.al>
2020-06-19 20:14:37 +0800

    Add binarycache

--- thanos.nix
+++ thanos.nix
@@ -195,6 +195,28 @@ in
           '';
         };
       };
+      "binarycache.home.monoid.al" = {
+        forceSSL = true;
+        enableACME = true;
+        extraConfig = ''
+          # To allow special characters in headers
+          ignore_invalid_headers off;
+          # Allow any size file to be uploaded.
+          client_max_body_size 0;
+          # To disable buffering
+          proxy_buffering off;
+        '';
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:${toString config.services.nix-serve.port}";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            '';
+          };
+        };
+      };
       "restic.home.monoid.al" = {
         forceSSL = true;
         enableACME = true;
